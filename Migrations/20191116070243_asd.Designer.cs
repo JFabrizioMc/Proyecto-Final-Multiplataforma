fafb910a3fad971aca457360b5bc240f44bf4564
joseaparicio <aldairxd11@hotmail.com>
2019-11-16 02:09:58 -0500

    :v

--- Migrations/20191116070243_asd.Designer.cs
+++ Migrations/20191116070243_asd.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Proyecto_Final_Multiplataforma.Models;
 
 namespace Proyecto_Final_Multiplataforma.Migrations
 {
     [DbContext(typeof(MongoContext))]
-    partial class MongoContextModelSnapshot : ModelSnapshot
+    [Migration("20191116070243_asd")]
+    partial class asd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/MongoContext.cs
+++ Models/MongoContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,32 @@ namespace Proyecto_Final_Multiplataforma.Models
                             Id = 3, 
                             Nombre = "Mouses"
                             });
+            modelBuilder.Entity<IdentityRole>()
+            .HasData(new IdentityRole{
+                Id = "ROLE_ID",
+                Name = "admin",
+                NormalizedName = "admin"
+
+            });
+            var hasher = new PasswordHasher<IdentityUser>();
+            modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser{
+                Id="ADMIN_ID",
+                UserName="admin@admin.com",
+                NormalizedUserName="aadmin@admin.com",
+                Email="admin@admin.com",
+                NormalizedEmail="admin@admin.com",
+                EmailConfirmed=true,
+                PasswordHash=hasher.HashPassword(null,"1234567"),
+                SecurityStamp = string.Empty
+             });
+
+             modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>{
+                 RoleId = "ROLE_ID",
+                 UserId = "ADMIN_ID"
+
+             });
+             
+            
 
             }
 
